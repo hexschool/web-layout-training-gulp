852b15b8e7a5bc6a2cccc86e065be4bbde225853
Ray <loveyou520babytw@gmail.com>
2020-05-06 12:48:42 +0800

    envOptions delete

--- gulpfile.js/envOptions.js
+++ gulpfile.js/envOptions.js
@@ -22,8 +22,6 @@ let envOptions = {
     path: distPath,
   },
   style: {
-    // 若您是撰寫 sass 就只需要將 *.scss 改成 *.sass 即可。
-    // 記得要修改位於 app/style/*.scss 的檔案唷。
     src: [
       `${srcPath}/assets/style/**/*.scss`,
       `${srcPath}/assets/style/**/*.sass`,
